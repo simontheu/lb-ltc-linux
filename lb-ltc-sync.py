9ada33a44edf3a14445723a74378f5c2fd5ee6f6
Simon Unsworth <simon@leobodnar.com>
2022-12-01 20:50:47 +0000

    changed port

--- lb-ltc-sync.py
+++ lb-ltc-sync.py
@@ -5,7 +5,7 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)      # For UDP
 
 udp_host = socket.gethostname()		        # Host IP
-udp_port = 12345			                # specified port to connect
+udp_port = 56753			                # specified port to connect
 
 #print type(sock) ============> 'type' can be used to see type 
 				# of any variable ('sock' here)
